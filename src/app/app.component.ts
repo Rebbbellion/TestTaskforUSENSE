0f55af0ccc15e12c30d59e306213bb510ba31647
Dzhosan Maksym <dzhomaks@gmail.com>
2024-03-06 12:00:03 +0200

    Alternative task completion with better a11y

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ type regExps = {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   public inputText: string = '';
   public inputMinLength: number = 8;
+  public passwordStatus: string | undefined = 'Field is empty';
 
   private regExps: regExps = {
     letters: /[a-zA-Z\u0400-\u04FF\s]+/,
@@ -23,7 +24,7 @@ export class AppComponent {
 
   private numberOfRegExpMatches: number = 0;
 
-  private regExpTest():void {
+  private regExpTest(): void {
     this.numberOfRegExpMatches = 0;
     for (const regexp in this.regExps) {
       if (this.regExps[regexp as keyof regExps].test(this.inputText)) {
@@ -34,18 +35,29 @@ export class AppComponent {
 
   private colors: Map<number, string> = new Map([
     [1, 'red'],
-    [2, '#eded1a'],
+    [2, '#c7c926'],
     [3, 'lime'],
     [4, 'gray'],
   ]);
 
-  public getPasswordStrength(sectionNumber: number): string | undefined {
+  private statuses: Map<number, string> = new Map([
+    [1, 'Weak password'],
+    [2, 'Middle strength password'],
+    [3, 'Strong password'],
+    [4, 'Password is too short'],
+    [5, 'Field is empty'],
+  ]);
+
+  public getPasswordStrength(sectionNumber: number = 0): string | undefined {
     this.regExpTest();
     if (this.inputText.length === 0) {
+      this.passwordStatus = this.statuses.get(5);
       return this.colors.get(4);
     } else if (this.inputText.length < this.inputMinLength) {
+      this.passwordStatus = this.statuses.get(4);
       return this.colors.get(1);
     } else if (this.numberOfRegExpMatches >= sectionNumber) {
+      this.passwordStatus = this.statuses.get(this.numberOfRegExpMatches);
       return this.colors.get(this.numberOfRegExpMatches);
     }
     return this.colors.get(4);
